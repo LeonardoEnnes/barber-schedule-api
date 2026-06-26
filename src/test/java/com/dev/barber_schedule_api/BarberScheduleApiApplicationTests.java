7ddd46faa6bb8f715a44e275bcadf9d45ad69cc7
Leonardo Ennes <leonardoennes7@gmail.com>
2026-06-26 17:48:58 -0300

    chore: organize project structure

---
+++ src/test/java/com/dev/barber_schedule_api/BarberScheduleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.barber_schedule_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberScheduleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
