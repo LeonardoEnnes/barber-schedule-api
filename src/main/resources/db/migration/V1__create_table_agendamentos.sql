b7c879cc6a7ad4c68f97a2a32770fa3a56030fa1
Leonardo Ennes <leonardoennes7@gmail.com>
2026-06-28 22:38:23 -0300

    feat: adicionando migration

---
+++ src/main/resources/db/migration/V1__create_table_agendamentos.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE agendamentos (
+    id UUID PRIMARY KEY,
+    cliente_id UUID NOT NULL,
+    prestador_id UUID NOT NULL,
+    data_hora_inicio TIMESTAMP WITH TIME ZONE NOT NULL,
+    data_hora_fim TIMESTAMP WITH TIME ZONE NOT NULL,
+    status VARCHAR(50) NOT NULL
+);
+
+CREATE INDEX idx_agendamentos_prestador_data ON agendamentos(prestador_id, data_hora_inicio, data_hora_fim);
