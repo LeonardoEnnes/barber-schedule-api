2b7ddb0430aec83e91a0e9e256044292359ff186
Leonardo Ennes <leonardoennes7@gmail.com>
2026-06-28 23:01:16 -0300

    feat: cria caso de uso de agendamento e repository ports

---
+++ src/main/java/com/dev/barber_schedule_api/application/ports/out/AgendamentoRepositoryPort.java
@@ -0,0 +1,10 @@
+﻿package com.dev.barber_schedule_api.application.ports.out;
+
+import com.dev.barber_schedule_api.core.domain.Agendamento;
+import java.time.Instant;
+import java.util.UUID;
+
+public interface AgendamentoRepositoryPort {
+    Agendamento save(Agendamento agendamento);
+    boolean existeConflitoDeHorario(UUID prestadorId, Instant inicio, Instant fim);
+}
---
+++ src/main/java/com/dev/barber_schedule_api/application/usecases/CriarAgendamentoUseCase.java
@@ -0,0 +1,25 @@
+﻿package com.dev.barber_schedule_api.application.usecases;
+
+import com.dev.barber_schedule_api.application.ports.out.AgendamentoRepositoryPort;
+import com.dev.barber_schedule_api.core.domain.Agendamento;
+import com.dev.barber_schedule_api.core.domain.Cliente;
+import com.dev.barber_schedule_api.core.domain.Prestador;
+import lombok.RequiredArgsConstructor;
+
+import java.time.Instant;
+
+@RequiredArgsConstructor
+public class CriarAgendamentoUseCase {
+
+    private final AgendamentoRepositoryPort agendamentoRepository;
+
+
+    public Agendamento executar(Cliente cliente, Prestador prestador, Instant inicio, Instant fim) {
+        if (agendamentoRepository.existeConflitoDeHorario(prestador.getId(), inicio, fim)) {
+            throw new IllegalArgumentException("O prestador já possui um agendamento neste horário.");
+        }
+        Agendamento novoAgendamento = new Agendamento(cliente, prestador, inicio, fim);
+        return agendamentoRepository.save(novoAgendamento);
+    }
+
+}
